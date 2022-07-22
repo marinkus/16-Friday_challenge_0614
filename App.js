4fd46d68eb0caa9e1f80f6140448b3083d4ed66a
marinkus <marinkus@gmail.com>
2022-07-22 14:18:56 +0300

    Clock

---
+++ App.js
@@ -0,0 +1,16 @@
+function currentTime() {
+  let clock = new Date(); 
+  let hh = clock.getHours();
+  let mm = clock.getMinutes();
+
+   hh = (hh < 10) ? '0' + hh : hh;
+   mm = (mm < 10) ? '0' + mm : mm;
+
+    
+   let time = hh + ":" + mm
+
+  document.getElementById("clock").innerText = time; 
+  let t = setTimeout(function(){ currentTime() }, 1000); 
+};
+
+currentTime();
